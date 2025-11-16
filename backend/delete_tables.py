a29473c3cc7760d9799a3b0277d4fe32fd62f4c6
Hasanov <gasanovdominik@gmail.com>
2025-11-16 21:55:52 +0100

    gg

--- backend/delete_tables.py
+++ backend/delete_tables.py
@@ -1,10 +1,12 @@
+from sqlalchemy import text
 from backend.database import engine
 
 def drop_tables():
     with engine.connect() as conn:
-        conn.execute("DROP TABLE IF EXISTS signal_deliveries CASCADE;")
-        conn.execute("DROP TABLE IF EXISTS signals CASCADE;")
+        print("Удаляю таблицы...")
+        conn.execute(text("DROP TABLE IF EXISTS signal_deliveries CASCADE;"))
+        conn.execute(text("DROP TABLE IF EXISTS signals CASCADE;"))
         conn.commit()
+        print("Таблицы удалены.")
 
 drop_tables()
-print("Таблицы signals и signal_deliveries удалены!")
